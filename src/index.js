88b87a70533a894b563b2f420e7061fe265c7e32
hackerman-rx <spyukkraher@gmail.com>
2022-05-10 21:32:36 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -10,8 +10,13 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+//Создать React app
+//Node command prompt =>
+//npx create-react-app название приложения
+//cd название приложения 
+//запускаем приложение
+//npm start
+
+//Установим пакетный менеджер yarn
+//npm i --global yarn
